cbefa8caf8341cbec25be125d55fb8f76f8fe75e
Patricia <82049947+Patrusky@users.noreply.github.com>
2022-06-06 12:06:32 +0200

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  nombre= '';
+  password = '';
+  allowNewServer = false;
+  serverCreated = false;
+  showSecret = false;
+
+  constructor (){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+  }
 }
